ffcf4e521d546b22761f50062f7b6b5b798f8e4c
duncan <duncanndiithi@yahoo.com>
2021-10-23 13:52:06 +0300

    add report template

--- resources/js/user/pt/reports/PTPerformanceReport.js
+++ resources/js/user/pt/reports/PTPerformanceReport.js
@@ -51,12 +51,84 @@ class PTPerformanceReport extends React.Component {
         const imgStyle = {
             // width: "70%"
         };
+        let tdtyle = {
+            textAlign: 'left',
+        }
+
+        let totalTableLength = 8;
         return (
             <React.Fragment>
-                <div id='agreementRates' ref={el => (this.componentRef = el)} >
-                    <img style={imgStyle} src={this.props.chart1}></img><br />
-                    Test print
-                </div>
+
+                <table className="unstrip table table-sm no-table-border" ref={el => (this.componentRef = el)}>
+                    <tbody >
+                        <tr >
+                            <td colSpan={totalTableLength}>
+                                <img style={imgStyle} src={this.props.chart1}></img>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={totalTableLength}>MINISTRY OF HEALTH</td>
+                        </tr>
+                        <tr>
+                            <td colSpan={totalTableLength}>NATIONAL PUBLIC HEALTH LABORATORIES</td>
+                        </tr>
+                        <tr>
+                            <td colSpan={totalTableLength}>KENYA EXTERNAL QUALITY ASSESSMENT SCHEME (KNEQAS)</td>
+                        </tr>
+                        <tr>
+                            <td colSpan={totalTableLength}>NATIONAL ONCOLOGY/BIOCHEMISTRY REFERENCE LABORATORY</td>
+                        </tr>
+                        <tr>
+                            <td colSpan={totalTableLength}>P.O Box 20750 - 00202 NAIROBI Email: nphlpt@nphl.go.ke Help Desk: helpdesk.nphl.go.ke</td>
+                        </tr>
+                        <tr><p></p></tr>
+                        <tr><p></p></tr>
+                        <tr style={{ "marginTop": "5px" }}>
+
+                            <td colSpan={3} style={tdtyle}>
+                                <div>Lab Code: 266</div>
+                                <div>Shipment Date 24-May-2021</div>
+                                <div>Specimen Receipt Date 31-May-2021</div>
+                                <div>Kit Lot Number</div>
+
+                            </td>
+                            <td colSpan={2} >
+                                <div>Lab Name: KEMRI P3</div>
+                                <div>Result Submission Date 04-Jun-2021</div>
+                                <div>Testing Date 08-Jun-2021</div>
+                                <div>Kit Expiration Date</div>
+
+                            </td>
+                            <td colSpan={3} style={{ 'textAlign': 'right' }}>
+                                <div>Phone No: +254722539294</div>
+                                <div>Platform: Roche CAP/CTM</div>
+                            </td>
+
+                        </tr>
+                        <tr><p></p></tr>
+                        <tr><p></p></tr>
+                        <tr>
+                            <td style={tdtyle} rowSpan={2}>SAMPLE </td>
+                            <td colSpan={2}>HR HPV 16 </td>
+                            <td colSpan={2}>HR HPV 18-45 </td>
+                            <td colSpan={2}>Others HR HPV</td>
+                            <td style={tdtyle} rowSpan={2}>PERFORMANCE</td>
+                        </tr>
+
+                        <tr>
+
+                            <td>Results</td>
+                            <td>Expected</td>
+
+                            <td>Results</td>
+                            <td>Expected</td>
+
+                            <td>Results</td>
+                            <td>Expected</td>
+
+                        </tr>
+                    </tbody>
+                </table>
 
                 <ReactToPrint
                     trigger={() => <button style={{ textAlign: 'center', marginLeft: '50%', backgroundColor: 'lemonchiffon', width: '62px' }}>Download</button>}
