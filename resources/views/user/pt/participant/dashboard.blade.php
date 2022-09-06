be7569fed21ef30530171a6d1820371bd40ad3d2
Benzer <benzerbett@gmail.com>
2022-09-06 10:28:52 +0300

    Permission validation in blades: fixed

--- resources/views/user/pt/participant/dashboard.blade.php
+++ resources/views/user/pt/participant/dashboard.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.participant')
 
+<?php
+    use Illuminate\Support\Facades\Gate;
+?>
 @section('content')
 <div class="container-fluid">
 
-    @if (Auth::user()->can('view_pt_component'))
-    <div id="participant-pt-dashboard"></div>
-    @endif
+    <?php if (Gate::allows('view_pt_component')) { ?>
+        <div id="participant-pt-dashboard"></div>
+    <?php } ?>
 
 </div>
 @endsection
--- resources/views/user/pt/participant/readiness.blade.php
+++ resources/views/user/pt/participant/readiness.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.participant')
 
+<?php
+    use Illuminate\Support\Facades\Gate;
+?>
+
 @section('content')
 <div class="container-fluid">
 
-    @if (Auth::user()->can('view_pt_component'))
-    <div id="readiness"></div>
-    @endif
+    <?php if (Gate::allows('view_pt_component')) { ?>
+        <div id="readiness"></div>
+    <?php } ?>
 
 </div>
 @endsection
